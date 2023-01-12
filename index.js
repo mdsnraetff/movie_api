caf8e16d122825e150c8a46e655526162aa96773
Madison <96503969+mdsnraetff@users.noreply.github.com>
2023-01-12 15:07:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express'),
   morgan = require('morgan');
+  fs = require('fs')
+  path = require('path')
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+app.use(morgan('combined', {stream: accessLogStream}));
+
 app.use(morgan('common'));
 
-
 app.use(express.static('public'));
+
 
 
 let topMovies = [
@@ -16,6 +21,7 @@ let topMovies = [
     }
 ]
 //add more movies
+
 
 
 
