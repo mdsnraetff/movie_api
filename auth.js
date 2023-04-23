9f9ea48612760927bf257c0605190331ecb997d1
Madison <96503969+mdsnraetff@users.noreply.github.com>
2023-04-23 17:35:25 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -17,7 +17,7 @@ let generateJWTToken = (user) => {
 
 /* POST login. */
 module.exports = (router) => {
-  app.post('/login', (req, res) => {
+  router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
