931019f5f352bc935321de2f1aaf85c3a71ced33
Madison <96503969+mdsnraetff@users.noreply.github.com>
2023-08-27 15:09:47 -0400

    Update passport.js

--- passport.js
+++ passport.js
@@ -10,36 +10,35 @@ let Users = Models.User,
 passport.use(new LocalStrategy({
     usernameField: "Username",
     passwordField: "Password",
-}, async (username, password, callback) => {
-    console.log(`${username} ${password}`);
-    await Users.findOne({ Username: username })
-        .then((user) => {
-            if (error) {
-                console.log(error);
-                return callback(error);
-            }
+}, (username, password, callback) => {
+    console.log(username + ' ' + password);
+    Users.findOne({ Username: username }, (error, user) => {
+        if (error) {
+            console.log(error);
+            return callback(error);
+        }
 
-            if (!user) {
-                console.log('incorrect username');
-                return callback(null, false, { message: 'Incorrect username.' });
-            }
+        if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, { message: 'Incorrect username.' });
+        }
 
-            if (!user.validatePassword(password)) {
-                console.log('incorrect password');
-                return callback(null, false, { message: 'Incorrect password.' });
-            }
+        if (!user.validatePassword(password)) {
+            console.log('incorrect password');
+            return callback(null, false, { message: 'Incorrect password.' });
+        }
 
-            console.log('finished');
-            return callback(null, user);
-        });
+        console.log('finished');
+        return callback(null, user);
+    });
 }));
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: 'your_jwt_secret'
 },
-    async (jwtPayload, callback) => {
-        return await Users.findById(jwtPayload._id).then((user) => {
+    (jwtPayload, callback) => {
+        return Users.findById(jwtPayload._id).then((user) => {
             return callback(null, user);
         })
             .catch((error) => {
